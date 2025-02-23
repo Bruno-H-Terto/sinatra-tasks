a5bf55954bffb4b5e59370c7b2f9ed63287a41b5
Bruno Herculano <herculanoterto@gmail.com>
2025-02-23 03:27:15 -0300

    Initial commit

---
+++ app.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require 'sinatra'
+require 'sinatra/activerecord'
+require 'dotenv'
+Dotenv.load
+
+set :database, {
+  adapter: 'sqlite3', database: ENV.fetch('DB_PATH', nil)
+}
